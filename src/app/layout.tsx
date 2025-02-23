d2754eb8e241787fd6116a10f48e4f0cf6d5044d
Kirill Eremin <kirill.eremins@yandex.ru>
2025-02-23 04:47:35 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Provider } from 'jotai';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
+import { Meta } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
 import './globals.css';
@@ -21,6 +22,11 @@ export const metadata: Metadata = {
     description: 'Home page',
 };
 
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+};
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -28,6 +34,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <Meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
             <body className={`${geistSans.variable} ${geistMono.variable}`}>
                 <Provider>{children}</Provider>
             </body>
